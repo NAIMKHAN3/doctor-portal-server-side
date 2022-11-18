ab2a07a661ea894869c559ca9733bc4fa50083d0
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-11-19 04:19:33 +0600

    create a api all users, and my appoinment

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const appoinmentOption = client.db('doctor-portal').collection('appoinment-option')
 const bookingCollection = client.db('doctor-portal').collection('booking')
+const usersCollection = client.db('doctor-portal').collection('users')
 async function run() {
     try {
         app.post('/booking', async (req, res) => {
@@ -36,23 +37,51 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
         })
+
+        app.post('/users', async (req, res) => {
+            try {
+                const user = req.body;
+                console.log(user)
+                const result = await usersCollection.insertOne(user);
+                res.send(result)
+            }
+            catch {
+                res.send({ status: false, message: 'cold not data' })
+            }
+        })
+
+        app.get('/allusers', async (req, res) => {
+            try {
+                const result = await usersCollection.find({}).toArray();
+                res.send(result)
+            }
+            catch {
+                res.send({ status: false, messege: 'conld not data' })
+            }
+        })
+
+        app.get('/myappoinment', async (req, res) => {
+            try {
+                const userEmail = req.query.email;
+                const result = await bookingCollection.find({ email: userEmail }).toArray();
+                res.send(result)
+            }
+            catch {
+                res.send({ status: false, messege: 'conld not data' })
+            }
+        })
+
         app.get('/appoinmentoption', async (req, res) => {
             const date = req.query.date;
-            // console.log(date)
             try {
                 const options = await appoinmentOption.find({}).toArray();
                 const alreadyBooked = await bookingCollection.find({ date: date }).toArray();
                 options.map(option => {
                     const optionBooked = alreadyBooked.filter(book => book.treetmentname === option.name)
-                    // console.log(optionBooked)
                     const bookedSlots = optionBooked.map(book => book.time)
                     const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
                     option.slots = remainingSlots;
-
-
-
                 })
-                // console.log(alreadyBooked)
                 res.send(options)
             }
             catch {
